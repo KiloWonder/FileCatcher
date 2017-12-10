49e4315e0cc06bad4b622d74d3cf238ddbe75a50
KnairWang <KnairWang@Outlook.com>
2017-12-10 11:32:26 +0800

    Update example.

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import file_catcher
 
 searcher = file_catcher.FileCatcher('C:\\')
 searcher\
-    .searchObjects(['user', 'programdata'], searcher.ObjectTypes.Folder, 0, searcher.SearchStrategy.StartWith, True)\
+    .searchObjects(['user', 'progr'], searcher.ObjectTypes.Folder, 0, searcher.SearchStrategy.StartWith, True)\
     .searchObjects('.exe', searcher.ObjectTypes.File, 2, searcher.SearchStrategy.EndWith)
 
 for item in searcher.lastSearchResult:
